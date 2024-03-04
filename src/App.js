786e90e149822231c9e0ed6370776d464830580c
Erin A <erinaviles83@gmail.com>
2024-03-04 18:28:51 -0500

    created initial files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.png"; 
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="Dictionary-logo img-fluid" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+
