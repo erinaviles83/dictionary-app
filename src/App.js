61c752645794a3381af824b473ad91ce34ff264a
Erin A <erinaviles83@gmail.com>
2024-03-09 21:04:58 -0500

    added event listener

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import logo from "./logo.png"; 
 import './App.css';
+import Dictionary from "./Dictionary"; 
 
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
         <img src={logo} className="Dictionary-logo img-fluid" alt="logo" />
       </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="App-footer"><small>Coded by Erin Aviles 📖</small>
+      </footer>
+    </div>
     </div>
   );
 }
